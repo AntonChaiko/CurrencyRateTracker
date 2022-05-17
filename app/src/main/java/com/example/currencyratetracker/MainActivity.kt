5add237fba6d38a191b8b62e6fe9c5eb9fa38423
ChaikoAnton <anton.ch@phpdev.org>
2022-05-17 23:30:26 +0300

    setup bottom navigation

--- app/src/main/java/com/example/currencyratetracker/MainActivity.kt
+++ app/src/main/java/com/example/currencyratetracker/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.currencyratetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.currencyratetracker.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavBar.setupWithNavController(navController)
+
     }
 }
