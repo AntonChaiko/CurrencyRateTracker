a9f67013ca65a2dae5097edafa13c9534a1029e4
ChaikoAnton <anton.ch@phpdev.org>
2022-05-18 00:02:53 +0300

    setup AutoCompleteTextView

--- app/src/main/java/com/example/currencyratetracker/ui/fragments/popularfragment/PopularFragment.kt
+++ app/src/main/java/com/example/currencyratetracker/ui/fragments/popularfragment/PopularFragment.kt
@@ -1,15 +1,26 @@
 package com.example.currencyratetracker.ui.fragments.popularfragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.currencyratetracker.R
 import com.example.currencyratetracker.databinding.PopularFragmentBinding
 import com.example.currencyratetracker.ui.fragments.basefragment.BaseFragment
 
 class PopularFragment : BaseFragment<PopularFragmentBinding>() {
+    @SuppressLint("ResourceType")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
+        val arrayAdapter = ArrayAdapter(
+            view.context, android.R.layout.simple_list_item_1, resources.getStringArray(R.array.currencies)
+        )
+        binding.currenciesTextView.setAdapter(arrayAdapter)
     }
     override fun initBinding(
         inflater: LayoutInflater,
